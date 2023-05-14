d9782e286ea11bf3ad6627cbd38cee9063f703db
ANASS-ANGRA <anassangra2003@gmail.com>
2023-05-14 11:59:06 +0100

    page login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import SignIn from "./login/Sign_in";
+
 
 function App() {
   return (
     <div className="App">
-      
+       <SignIn/>
     </div>
   );
 }
---
+++ src/login/Sign_in.js
@@ -0,0 +1,133 @@
+import React ,{useState} from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+
+
+const theme = createTheme();
+
+export default function SignIn() {
+   
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  function validateEmail() {
+    // This regular expression checks if the email is in a valid format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setEmailError('Please enter a valid email address');
+      alert("Please enter a valid email address");
+    } else {
+      setEmailError('');
+    }
+  }
+
+  function validatePassword() {
+    if (password.length < 8) {
+      setPasswordError('Password must be at least 8 characters long');
+      alert('Password must be at least 8 characters long')
+    } else {
+      setPasswordError('');
+    }
+  }
+
+
+  const handleSubmit = (event) => {
+        event.preventDefault()
+       validateEmail()
+       validatePassword()
+  }
+ 
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form"  onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={email}
+              onChange={(e)=>{setEmail(e.target.value)}}
+            />
+            
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e)=>{setPassword(e.target.value)}}
+            />
+      
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+            </Grid>
+            {emailError &&    <Grid container>
+              <Grid item xs>
+                  {emailError}
+
+              </Grid>
+            </Grid>}
+            {passwordError &&    <Grid container>
+              <Grid item xs>
+                  {passwordError}
+
+              </Grid>
+            </Grid>}
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  )
+        }
